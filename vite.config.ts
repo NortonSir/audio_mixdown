6d3b43adcef6f88a46a37cb02db6c355fd6ae17c
NortonSir <nortonsir@gmail.com>
2025-11-23 20:40:59 +0900

    Refactor: Remove unused path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,6 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
       }
     };
 });
